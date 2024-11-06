38e40d7da350eb8542361c91cfb0106c7f71be9a
andre <andre.marcos@gmail.com>
2024-11-06 18:26:57 -0300

    fechamento versao par aprs

--- main.py
+++ main.py
@@ -2,20 +2,28 @@ import requests
 
 # URL da sua API
 
-#url = 'http://localhost:5000/wlist/get'  # Altere se necessário
-#url = 'http://localhost:5000/wlist/getfull'  # Altere se necessário
-url = "https://faccamp.pythonanywhere.com/wlist/get"
+#url = 'http://localhost:5000/hangman-api/getword'  # Altere se necessário
+#url = 'http://localhost:5000/hangman-api/getdata'  # Altere se necessário
+#url = 'http://localhost:5000/hangman-api/getlist'  # Altere se necessário
+url = 'http://localhost:5000/hangman-api/getlist/2'  # Altere se necessário
+#url = "https://faccamp.pythonanywhere.com/hangman-api/getword"
 
 def get_message():
     try:
         response = requests.get(url)
         response.raise_for_status()  # Levanta um erro se a resposta for um erro HTTP
         data = response.json()  # Converte a resposta para JSON
-        
-        print("Mensagem recebida, COMPLETA:", data)
-        print("Mensagem recebida, PALAVRA:", data['palavra'])
-        ##print("Mensagem recebida, CATEGORIA:", data['categoria'])
-        ##print("Mensagem recebida, COMPLEXIDADE:", data['complexidade'])
+    
+        print("Mensagem recebida, TIPO:", type(data))
+        print("Mensagem recebida, DATA:", data)
+
+        if type(data) == list :
+            for item in data :
+                print(f"{item['palavra']},{item['categoria']},{item['complexidade']}")
+        else :
+            print("Mensagem recebida, PALAVRA:", data['palavra'])
+            ##print("Mensagem recebida, CATEGORIA:", data['categoria'])
+            ##print("Mensagem recebida, COMPLEXIDADE:", data['complexidade'])
 
     except requests.exceptions.RequestException as e:
         print("Erro ao acessar a API:", e)
